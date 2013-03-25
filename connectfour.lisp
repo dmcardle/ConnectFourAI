40a5a5e3817197a48826c38f9ca168ce6449f682
Dan McArdle <zingermc@gmail.com>
2013-03-24 21:02:38 -0400

    Added defense strategy and randomized offense strategy. Printing with terminal colors, too.

--- connectfour.lisp
+++ connectfour.lisp
@@ -13,6 +13,7 @@
 ;;; ---------------
 
 (defvar *player1Goes* t)
+(defvar *roboPlayer* -1) ;; which player is robot?
 (defvar *numRows* 6)
 (defvar *numCols* 7)
 
@@ -38,11 +39,26 @@
 
 (defun printRow (row)
     "Print a single list (x0 x1 ... xn) like  | x0 | x1 | ... | xn |"
+
+    ;; printing using terminal colors
+    ;; see http://wynnnetherland.com/journal/a-stylesheet-author-s-guide-to-terminal-colors
+
     (cond
         ((null row)
-            (format t "|~%"))
+            (format t "~C[00m|~%" #\Esc))
         (t (progn
-            (format t "| ~S " (car row))
+            (cond
+                ((eq (car row) '~)
+                    (format t "~C[00m| - " #\Esc))
+                (t
+		 ;(format t "| ~S " (car row))
+		 (cond
+		   ((eq (car row) 'X)
+		    (format t "~C[00m| ~C[33mX " #\Esc #\Esc))
+		   ((eq (car row) 'O)
+		    (format t "~C[00m| ~C[31mO " #\Esc #\Esc))
+		   (t (format t "~C[00m| ~S " #\Esc (car row))))
+		 ))
             (printRow (cdr row))))))
 
 
@@ -62,8 +78,8 @@
 (defun makeMove (board colNum)
     ;; decide which symbol to use
     (setf sym (cond
-        (*player1Goes* 'X)
-        (t 'O)))
+        (*player1Goes* 'O)
+        (t 'X)))
 
     ;; adjust for numbers displayed to user starting at 1
     (setf colNum (- colNum 1))
@@ -86,10 +102,7 @@
         ;; else
         (progn
             (setf rowNum (- (- *numRows* rowNum) 1)) 
-            (setf (nth colNum (nth rowNum board)) sym)))
-
-    ;; other player's turn now
-    (setf *player1Goes* (not *player1Goes*)))
+            (setf (nth colNum (nth rowNum board)) sym))))
 
 
 (defun checkGameOver (board)
@@ -163,12 +176,84 @@
   (if (and
         (numberp val)
         (> val 0)
-        (<= val *numCols*))  val
+        (<= val *numCols*))
+      (progn
+	;; other player's turn now
+	(setf *player1Goes* (not *player1Goes*))
+	val)
 
     ;; otherwise
        (progn
         (format t "Please enter a number between 1 and ~S.~%" *numCols*)
         (askUserForCol)))))
+
+(defun askRobotForCol (board)
+
+  ;;                           D E F E N S E
+  ;;    take the move that will make us win
+  ;;        OR
+  ;;    take the move that will prevent the other player from winning
+  (defun findWinningCol ()
+    "For each column, move there and see if the game ends in a win"
+
+    (defun winningCol (n)
+      ;; create a copy of the current board to experiment on
+      (setf sandbox (mapcar #'copy-list board))
+
+      ;; if we are not testing row 0
+      (if (not (eq n 0))
+	  (progn
+	    
+	    (makeMove sandbox n)
+	    
+	    (setf gameOverP (checkGameOver sandbox))
+	    
+	    ;; if the game is not a draw and this move ended the game
+	    (if (and (not (eq gameOverP 'DRAW)) gameOverP)
+		;; return this column
+		n
+		;; otherwise, try again on the next column
+		(winningCol (- n 1))))
+
+	  ;; otherwise, admit that we don't know what to do
+	  0))
+    
+    (winningCol *numCols*))
+
+
+  ;; check for winning move for us
+  (setf immediateWin (findWinningCol))  
+
+
+
+  ;; switch move to other player, make moves on pretend board as him
+  ;; this will have to be done anyway and it allows us to pretend the other player is moving
+  (setf *player1Goes* (not *player1Goes*))
+
+  ;; check for winning move for other player
+  (setf blockingMove (findWinningCol))
+
+
+  ;;                           O F F E N S E
+  ;;    plan for victory
+  ;; 
+  ;; TODO implement Victor Allis' rules for perfect play
+  (defun offenseChoice ()
+    (+ 1 (random *numCols*)))
+
+
+
+  (cond
+    ;; first choice is the winning move for us, if it exists
+    ((not (eq immediateWin 0))
+      immediateWin)
+
+    ;; otherwise, best choice is the move that prevents other player from winning
+    ((not (eq blockingMove 0))
+     blockingMove)
+    
+    ;; there's no way to win or screw the other guy, time for OFFENSE strategy
+    (t (offenseChoice))))
 
 (defun takeTurn (board )
 
@@ -179,7 +264,16 @@
             (t  "~%          PLAYER 2 (O)~%")))
 
     (printBoard board)
-    (setf colChoice (askUserForCol))
+
+    (cond
+      ;; robot's turn
+      ((or
+	(and (eq *roboPlayer* 1) *player1Goes*)
+	(and (eq *roboPlayer* 2) (not *player1Goes*)))
+       (setf colChoice (askRobotForCol board)))
+      ;; user's turn
+      (t (setf colChoice (askUserForCol))))
+  
     (makeMove board colChoice)
 
     ;; recurse
@@ -200,7 +294,28 @@
         (t (takeTurn board))))
 
 
-(format t "~%~%     LET THE GAMES BEGIN!~%~%")
-(setf board (createBoard *numCols* *numRows*))
-(takeTurn board)
+(defun decideRobot ()
+  "Ask user who should be the robot player. Sets value in global *roboPlayer* variable."
+  (format t "Choose which player is robot.~%~
+   Enter 0 for human vs. human game.~%~
+   Enter 1 for robot vs. human game.~%~
+   Enter 2 for human vs. robot game.~%::")
+  (setf robbie (read))
+  (if (not (and (>= robbie 0) (<= robbie 2)))
+      (progn
+	(format t "--- Invalid input.  Enter either 0, 1, or 2~%")
+	(decideRobot))
+      ;; otherwise
+      (setf *roboPlayer* robbie)))
 
+
+(defun main ()
+  (format t "~%~%     SETUP~%~%")
+  (decideRobot)
+
+  (format t "~%~%     LET THE GAMES BEGIN!~%~%")
+  (setf board (createBoard *numCols* *numRows*))
+  (takeTurn board))
+
+
+(main)
